1a254c6e5ac4e6f4853d89309aff305b8473a4ec
Antoine Cheron <15834744+AntoineCheron@users.noreply.github.com>
2021-11-30 11:44:02 +0100

    clean

--- notion-github/src/pages/Content/index.js
+++ notion-github/src/pages/Content/index.js
@@ -1,10 +1,4 @@
-import { printLine } from './modules/print';
 import { isGithubPR, getNotionLink } from './modules/github';
-
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
 
 if (isGithubPR()) {
   const notionLink = getNotionLink()
