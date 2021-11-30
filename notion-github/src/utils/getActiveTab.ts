0edd80379175c97754ad77f89d2664cc15951b33
Antoine Cheron <15834744+AntoineCheron@users.noreply.github.com>
2021-11-30 10:58:26 +0100

    Update boilerplate

--- notion-github/src/utils/getActiveTab.ts
+++
@@ -1,9 +0,0 @@
-import browser, { Tabs } from "webextension-polyfill";
-
-export async function getActiveTab(): Promise<Tabs.Tab | undefined> {
-    const tabs = await browser.tabs.query({
-        active: true,
-        currentWindow: true,
-    });
-    return tabs[0];
-}
