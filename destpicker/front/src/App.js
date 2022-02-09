0aa9757c2d6978ea32139c2e06f7bae33f9663e7
Antoine Cheron <15834744+AntoineCheron@users.noreply.github.com>
2022-02-09 18:27:31 +0100

    v1

--- destpicker/front/src/App.js
+++ destpicker/front/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Button, Col, Form, Input, Row, Typography } from 'antd'
+import { Button, Col, Form, Input, Row, Typography, Space, Spin } from 'antd'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 
 import { Destinations } from './Destinations'
 import { destpicker } from './api'
@@ -8,56 +9,127 @@ import './App.css'
 
 const { Paragraph, Title } = Typography
 
-const EXAMPLE_API_RESULT = [
-  {
-    destination: { name: 'Chamonix', address: 'Chamonix' },
-    average_duration_second: 1500,
-    average_distance_meter: 1500,
-    max_duration_second: 2000,
-    max_distance_meter: 2000,
-    journeys: [
-      {
-        participant: { name: 'Marco', address: 'Paris' },
-        distance_meter: 1000,
-        duration_second: 1000,
-      },
-      {
-        participant: { name: 'Antoine', address: 'Lyon' },
-        distance_meter: 2000,
-        duration_second: 2000,
-      }
-    ]
-  },
-  {
-    destination: { name: 'Biarritz', address: 'Biarritz' },
-    average_duration_second: 2500,
-    average_distance_meter: 2500,
-    max_duration_second: 3000,
-    max_distance_meter: 3000,
-    journeys: [
-      {
-        participant: { name: 'Marco', address: 'Paris' },
-        distance_meter: 3000,
-        duration_second: 3000,
-      },
-      {
-        participant: { name: 'Antoine', address: 'Lyon' },
-        distance_meter: 2000,
-        duration_second: 2000,
-      }
-    ]
-  }
-]
-
 function App() {
+  const onSubmit = async (values) => {
+    console.log(values)
+    setIsLoading(true)
+    try {
+      const destinations = values.destinations.map(d => ({ name: d.value, address: d.value }))
+      const response = await destpicker(values.participants, destinations)
+      setTransitSummary(response.data)
+      console.log({ apiData: response.data }) // TODO remove
+    } catch (error) {
+      alert("Oh nooooooo, an error happened while calling the API. Take a look at the console.")
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [transitSummary, setTransitSummary] = React.useState(null)
+
+
   return (
     <>
       <Row className="main-container" gutter={8}>
         <Col className="gutter-row" span={12}>
-          <DestPickerCard />
+          <Card>
+            <Row>
+              <Col span={24}>
+                <Title level={2}>Eric.ai 🧠</Title>
+                <Paragraph>
+                  Say hello to Eric.ai 👋. He will help you organize the next off-sites!<br />
+                  Give him the name and address of every team member you want to gather and he'll tell you where is the best place to meet.
+                </Paragraph>
+              </Col>
+            </Row>
+
+            <Row className="margin-top-32">
+              <Col span={24}>
+                <Form onFinish={onSubmit} autoComplete="off" layout="vertical">
+                  <Title level={3}>Off-site participants</Title>
+
+                  <Form.List name="participants">
+                    {(fields, { add, remove }) => (
+                      <>
+                        {fields.map(({ key, name, ...restField }) => (
+                          <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                            <Form.Item
+                              {...restField}
+                              name={[name, 'name']}
+                              rules={[{ required: true, message: 'Missing name' }]}
+                            >
+                              <Input placeholder="Name" />
+                            </Form.Item>
+                            <Form.Item
+                              {...restField}
+                              name={[name, 'address']}
+                              rules={[{ required: true, message: 'Missing address' }]}
+                            >
+                              <Input placeholder="Address" />
+                            </Form.Item>
+                            <MinusCircleOutlined onClick={() => remove(name)} />
+                          </Space>
+                        ))}
+                        <Form.Item>
+                          <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                            Add participant
+                          </Button>
+                        </Form.Item>
+                      </>
+                    )}
+                  </Form.List>
+
+                  <Title level={3}>Destination candidates</Title>
+
+                  <Form.List name="destinations">
+                    {(fields, { add, remove }) => (
+                      <>
+                        {fields.map(({ key, name, ...restField }) => (
+                          <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                            <Form.Item
+                              {...restField}
+                              name={[name, 'value']}
+                              rules={[{ required: true, message: 'Missing name' }]}
+                            >
+                              <Input placeholder="Name" />
+                            </Form.Item>
+                            <MinusCircleOutlined onClick={() => remove(name)} />
+                          </Space>
+                        ))}
+                        <Form.Item>
+                          <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                            Add destination
+                          </Button>
+                        </Form.Item>
+                      </>
+                    )}
+                  </Form.List>
+
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                      Submit
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Col>
+            </Row>
+          </Card>
         </Col>
         <Col className="gutter-row" span={12}>
-          <Card className="defacto-img-background" />
+          {transitSummary === null ? <Card className="defacto-img-background" /> : (
+            <Card>
+              <Row className="margin-top-32">
+                <Col span={24}>
+                  <Title level={3}>Results !!!</Title>
+                </Col>
+                <Col span={24}>
+                  {isLoading ? <Spin size="large" /> : (<Destinations apiResult={transitSummary} />)}
+                </Col>
+              </Row>
+            </Card>
+          )}
         </Col>
       </Row>
     </>
@@ -66,66 +138,6 @@ function App() {
 
 function Card({ className, children }) {
   return <div className={`${className} card`}>{children}</div>
-}
-
-function DestPickerCard() {
-  const onSubmit = (values) => {
-    console.log('Success:', values);
-  };
-
-  const [participants, setParticipants] = React.useState([{ 'name': 'Marco', 'address': 'Paris' }, { 'name': 'Antoine', address: 'rue de marseille 69007 Lyon' }])
-  const [destinations, setDestinations] = React.useState([{ 'name': 'Chamonix', 'address': 'Chamonix' }, { 'name': 'Biarrtz', address: 'Biarritz' }])
-
-  destpicker(participants, destinations).then(console.log)
-
-  return (
-    <Card>
-      <Row>
-        <Col span={24}>
-          <Title level={2}>Eric.ai </Title>
-          <Paragraph>
-            Say hello to Eric.ai 👋. He will help you organize the next off-sites!<br />
-            Give him the name and address of every team member you want to gather and he'll tell you where is the best place to meet.
-          </Paragraph>
-        </Col>
-      </Row>
-
-      <Row className="margin-top-32">
-        <Col span={24}>
-          <Title level={3}>Off-site participants</Title>
-          <Form onFinish={onSubmit} autoComplete="off" labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}>
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-              <Input.Password />
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
-
-      <Row className="margin-top-32">
-        <Col span={24}>
-          <Destinations apiResult={EXAMPLE_API_RESULT} />
-        </Col>
-      </Row>
-    </Card>
-  )
 }
 
 export default App
--- destpicker/front/src/Destinations.js
+++ destpicker/front/src/Destinations.js
@@ -7,16 +7,17 @@ const { Paragraph } = Typography
 // antd doc: https://ant.design/components/form/
 
 function duration(seconds) {
-  return moment.utc(seconds*1000).format('HH:mm:ss');
+  return moment.utc(seconds * 1000).format('HH:mm:ss');
 }
 function distance(d) {
-  return `${d/1000}km`;
+  return `${d / 1000}km`;
 }
 
 const columns = [
-  { title: 'Destination', dataIndex: ['destination', 'name'], key: 'name'},
-  { title: 'Avg Duration', dataIndex: 'average_duration_second', render: (a) => duration(a) },
-  { title: 'Avg Distance', dataIndex: 'average_distance_meter', render: (a) => distance(a) },
+  { title: 'Destination', dataIndex: ['destination', 'name'], key: 'name' },
+  { title: 'Avg Duration', dataIndex: 'average_duration_second', render: (a) => duration(a), key: 'avg_duration' },
+  { title: 'Max Duration', dataIndex: 'max_duration_second', render: (a) => duration(a), key: 'max_duration' },
+  { title: 'Avg Distance', dataIndex: 'average_distance_meter', render: (a) => distance(a), key: 'avg_distance' },
 ];
 
 
@@ -24,7 +25,7 @@ export function Destinations({ apiResult }) {
   return <Table
     columns={columns}
     expandable={{
-      expandedRowRender: record => <Journey journeys={record.journeys}/>,
+      expandedRowRender: record => <Journey journeys={record.journeys} />,
     }}
     dataSource={apiResult}
   />
@@ -33,7 +34,7 @@ export function Destinations({ apiResult }) {
 
 // apiResult is the result from the API, it is never null
 const journey_columns = [
-  { title: 'Participant', dataIndex: ['participant', 'name']},
+  { title: 'Participant', dataIndex: ['participant', 'name'] },
   { title: 'duration', dataIndex: 'duration_second', render: (a) => duration(a) },
   { title: 'distance', dataIndex: 'distance_meter', render: (a) => distance(a) },
 ];
