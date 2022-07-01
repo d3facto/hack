c2c2a2af480a93c8ccdd11fbfe1948ef8d9741c8
ARIC LASRY <aric@arics-mbp.home>
2022-07-01 16:28:53 +0200

    Load SIREN from URI

--- widget-ui/src/App.js
+++ widget-ui/src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Widget from './Widget'
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Widget siren={"552120222"} />
-    </div>
-  );
+  const [siren, setSiren] = useState(null)
+  useEffect(() => {
+    const url = new URL(
+     window.location
+    );
+    const value = url.searchParams.get('siren'); // => 'hello'
+    setSiren(value)
+    
+
+  }, [])
+
+  console.log('siren IS " :', siren)
+  if (!siren) {
+    return (<div>Defacto...</div>)
+  } else {
+
+    return (
+      <div className="App">
+        <Widget siren={siren} />
+      </div>
+    );
+  }
 }
 
 export default App;
