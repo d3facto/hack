00075f2b568363d4c54e866ccd49b2c509ad8136
ARIC LASRY <aric@arics-mbp.home>
2022-07-01 14:49:54 +0200

    Adding Widget UI boilerplate code

---
+++ widget-ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Widget from './Widget'
+
+function App() {
+  return (
+    <div className="App">
+      <Widget siren={"abc"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ widget-ui/src/Widget.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function Widget(siren) {
+    // const [...] = useState()
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div></div>
+    );
+  }
+  
+  export default Widget;
+  
