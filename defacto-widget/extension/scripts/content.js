7866b530b83c1be027bdcea8fd8cc063d962a6a7
Julien Gilli <jgilli@fastmail.fm>
2022-07-01 16:09:19 +0200

    WIP

--- defacto-widget/extension/scripts/content.js
+++ defacto-widget/extension/scripts/content.js
@@ -1,30 +1,32 @@
 'use strict';
 
 console.log('FOO')
-const containerId = 'github-chat-container'
-const boxId = 'github-chat-box'
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('FOO')
   createOverlay()
 })
 
+function getSiren() {
+  return document.getElementById('identite-siren')
+}
+
 function createOverlay() {  
-  const iframe = createiFrame()
-  headerElem = document.body.getElementById('header')
+  const sirenElem = document.getElementById('identite-siren')
+  const siren = sirenElem.textContent.replaceAll(/\s+/g, '')  
+  const iframe = createiFrame(siren)  
   document.body.appendChild(iframe)
 }
 
-function createiFrame() {
+function createiFrame(siren) {
   let baseUrl = chrome.runtime.getURL('iframe.html')
-  // baseUrl = encodeURIComponent(baseUrl)
   let iframe = document.createElement('iframe')
-  iframe.id = 'github-chat-box-iframe-wrapper'
+  iframe.id = 'defacto-widget'
   
   iframe.style.width = '100%'
   iframe.style.height = '100%'
 
-  iframe.src = `${baseUrl}`
+  iframe.src = `https://d3facto.github.io/hack/?siren=${siren}`
 
   return iframe
 }
